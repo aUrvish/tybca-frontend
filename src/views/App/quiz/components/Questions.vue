94485305c3d275d47f66e038c6340600e56f4e9b
vaghelavivek <vaghelavivek150@gmail.com>
2024-02-14 16:12:36 +0530

    added transition on questions section

--- src/views/App/quiz/components/Questions.vue
+++ src/views/App/quiz/components/Questions.vue
@@ -18,7 +18,7 @@ const questions = ref(
                 }
             ],
             isRequire: false,
-        },
+        }
     ]
 );
 
@@ -75,92 +75,96 @@ const chandeDragIndex = (e) => {
 
 <template>
     <div class="max-w-screen-lg mx-auto">
-        <VueDraggableNext tag="div" v-model="questions" @end="chandeDragIndex">
-            <div class="px-10 mb-4 relative" v-for="(que, index) in questions" :key="index">
-                <div class="p-5 border border-l-[5px] border-l-primary bg-white rounded-lg  cursor-move group">
-                    <p class="absolute left-0 text-[32px] font-bold text-primary -translate-x-1/2">{{ que.defaultIndex + 1 }}</p>
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                        class="max-w-6 fill-gray-400 absolute top-1/2 left-0 -translate-x-1/2 -translate-y-1/2 invisible group-hover:visible"
-                        viewBox="0 0 448 512">
-                        <path
-                            d="M32 288c-17.7 0-32 14.3-32 32s14.3 32 32 32l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L32 288zm0-128c-17.7 0-32 14.3-32 32s14.3 32 32 32l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L32 160z" />
-                    </svg>
-                    <div class="flex justify-between items-center">
-                        <div>
-                            <input type="text" class="text-lg font-semibold outline-0" v-model="questions[index].title">
-                            <p class="text-gray-400 text-sm -mt-1">Title</p>
+        <VueDraggableNext v-model="questions" @end="chandeDragIndex">
+            <transition-group type="transition" name="fade">
+                <div class="px-10 mb-4 relative" v-for="(que, index) in questions" :key="que.defaultIndex">
+                    <div class="p-5 border border-l-[5px] border-l-primary bg-white rounded-lg  cursor-move group">
+                        <p class="absolute left-0 text-[32px] font-bold text-primary -translate-x-1/2">{{ que.defaultIndex +
+                            1 }}</p>
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                            class="max-w-6 fill-gray-400 absolute top-1/2 left-0 -translate-x-1/2 -translate-y-1/2 invisible group-hover:visible"
+                            viewBox="0 0 448 512">
+                            <path
+                                d="M32 288c-17.7 0-32 14.3-32 32s14.3 32 32 32l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L32 288zm0-128c-17.7 0-32 14.3-32 32s14.3 32 32 32l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L32 160z" />
+                        </svg>
+                        <div class="flex justify-between items-center">
+                            <div>
+                                <input type="text" class="text-lg font-semibold outline-0" v-model="questions[index].title">
+                                <p class="text-gray-400 text-sm -mt-1">Title</p>
+                            </div>
+                            <!-- v-click-outside="() => isDropIndex = index" -->
+                            <div class="max-w-48 w-full relative select-none">
+                                <div class="flex justify-between items-center cursor-pointer border py-1.5 px-3 rounded-md"
+                                    @click="dropdownToggle(index)" v-click-outside="() => dropdownClose(index)">
+                                    <h5 class="capitalize">{{ que.type }}</h5>
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="max-w-2.5 fill-gray-400"
+                                        viewBox="0 0 320 512">
+                                        <path
+                                            d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z" />
+                                    </svg>
+                                </div>
+                                <ul class="absolute mt-3 rounded-md bg-white border p-1.5 w-full"
+                                    :class="isDropDownShow[index] ? '' : 'hidden'">
+                                    <li class="py-2 px-3 hover:bg-gray-100 rounded-md cursor-pointer capitalize"
+                                        v-for="(item, ind) in dropItem" :key="ind" @click="setDropdownItem(index, item)">{{
+                                            item
+                                        }}
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                        <!-- v-click-outside="() => isDropIndex = index" -->
-                        <div class="max-w-48 w-full relative select-none">
-                            <div class="flex justify-between items-center cursor-pointer border py-1.5 px-3 rounded-md"
-                                @click="dropdownToggle(index)" v-click-outside="() => dropdownClose(index)">
-                                <h5 class="capitalize">{{ que.type }}</h5>
-                                <svg xmlns="http://www.w3.org/2000/svg" class="max-w-2.5 fill-gray-400"
-                                    viewBox="0 0 320 512">
+
+                        <div class="grid grid-cols-2 gap-5 mt-10">
+                            <div class="flex items-center justify-between border rounded-md py-3 px-4 group-option"
+                                v-for="(option, ind) in que.option" :key="ind"
+                                :class="option.isCorrect ? 'border-green-400' : ''">
+                                <div class="grow items-center flex">
+                                    <input :name="`que${index}`" type="radio" class="w-4 h-4" v-if="que.type == 'options'"
+                                        :checked="ind == 0" disabled>
+                                    <input :name="`que${index}`" type="checkbox" class="w-4 h-4"
+                                        v-else-if="que.type == 'checkboxes'" :checked="ind == 0" disabled>
+                                    <p class="font-semibold text-gray-500" v-else>1.</p>
+                                    <input class="ml-3 outline-0" v-model="questions[index].option[ind].name">
+                                </div>
+
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
+                                    class="max-w-4 ml-3 cursor-pointer group-option-svg invisible" :class="questions[index].option[ind].isCorrect
+                                        ? 'fill-green-400' : 'fill-gray-300'" @click="isCorrectOption(index, ind)">
                                     <path
-                                        d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z" />
+                                        d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z" />
+                                </svg>
+
+                                <svg xmlns="http://www.w3.org/2000/svg"
+                                    class="max-w-4 ml-3 fill-gray-400 cursor-pointer group-option-svg invisible"
+                                    viewBox="0 0 384 512" @click="removeOption(index)">
+                                    <path
+                                        d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
                                 </svg>
                             </div>
-                            <ul class="absolute mt-3 rounded-md bg-white border p-1.5 w-full"
-                                :class="isDropDownShow[index] ? '' : 'hidden'">
-                                <li class="py-2 px-3 hover:bg-gray-100 rounded-md cursor-pointer capitalize"
-                                    v-for="(item, ind) in dropItem" :key="ind" @click="setDropdownItem(index, item)">{{
-                                        item
-                                    }}
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div class="grid grid-cols-2 gap-5 mt-10">
-                        <div class="flex items-center justify-between border rounded-md py-3 px-4 group-option"
-                            v-for="(option, ind) in que.option" :key="ind"
-                            :class="option.isCorrect ? 'border-green-400' : ''">
-                            <div class="grow items-center flex">
-                                <input :name="`que${index}`" type="radio" class="w-4 h-4" v-if="que.type == 'options'"
-                                    :checked="ind == 0" disabled>
-                                <input :name="`que${index}`" type="checkbox" class="w-4 h-4"
-                                    v-else-if="que.type == 'checkboxes'" :checked="ind == 0" disabled>
-                                <p class="font-semibold text-gray-500" v-else>1.</p>
-                                <input class="ml-3 outline-0" v-model="questions[index].option[ind].name">
+                            <div class="flex items-center cursor-pointer" @click="addOption(index)">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="max-w-5 fill-gray-400" viewBox="0 0 512 512">
+                                    <path
+                                        d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z" />
+                                </svg>
+                                <p class="text-sm ml-2 text-gray-400">Add more</p>
                             </div>
+                        </div>
 
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
-                                class="max-w-4 ml-3 cursor-pointer group-option-svg invisible" :class="questions[index].option[ind].isCorrect
-                                    ? 'fill-green-400' : 'fill-gray-300'" @click="isCorrectOption(index, ind)">
+                        <div class="mt-10 flex items-center justify-end">
+                            <div class="flex items-center">
+                                <input type="checkbox" class="h-5 w-5 mr-3" id="require1"
+                                    v-model="questions[index].isRequire">
+                                <label for="require1">Required</label>
+                            </div>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="max-w-4 fill-red-400 ml-5 cursor-pointer"
+                                viewBox="0 0 448 512" @click="removeQuestion(index)">
                                 <path
-                                    d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z" />
-                            </svg>
-
-                            <svg xmlns="http://www.w3.org/2000/svg"
-                                class="max-w-4 ml-3 fill-gray-400 cursor-pointer group-option-svg invisible"
-                                viewBox="0 0 384 512" @click="removeOption(index)">
-                                <path
-                                    d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+                                    d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
                             </svg>
                         </div>
-                        <div class="flex items-center cursor-pointer" @click="addOption(index)">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="max-w-5 fill-gray-400" viewBox="0 0 512 512">
-                                <path
-                                    d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z" />
-                            </svg>
-                            <p class="text-sm ml-2 text-gray-400">Add more</p>
-                        </div>
-                    </div>
-
-                    <div class="mt-10 flex items-center justify-end">
-                        <div class="flex items-center">
-                            <input type="checkbox" class="h-5 w-5 mr-3" id="require1" v-model="questions[index].isRequire">
-                            <label for="require1">Required</label>
-                        </div>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="max-w-4 fill-red-400 ml-5 cursor-pointer"
-                            viewBox="0 0 448 512" @click="removeQuestion(index)">
-                            <path
-                                d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
-                        </svg>
                     </div>
                 </div>
-            </div>
+            </transition-group>
         </VueDraggableNext>
         <div class="px-10">
             <div class="border-4 border-dashed py-10 grid place-items-center cursor-pointer border-primary rounded-md"
