7f1990ca843766bac37e5e51145775479b685431
Urvish <mandaliyaurvishj7@gmail.com>
2024-02-15 00:10:40 +0530

    added table component in user section

---
+++ src/views/App/students/components/UserTable.vue
@@ -0,0 +1,149 @@
+<script setup>
+import Btn from '@/components/Btn.vue';
+import { ref } from 'vue';
+const props = defineProps({
+    max: {
+        default: 5,
+    }
+})
+const isShowActionMenu = ref(null)
+const showActionMenu = (index) => {
+    if (isShowActionMenu.value == index) {
+        isShowActionMenu.value = null
+        return null
+    }
+    isShowActionMenu.value = index
+}
+
+const hideActionMenu = (index) => {
+    if (isShowActionMenu.value == index) {
+        isShowActionMenu.value = null
+    }
+}
+
+</script>
+
+<template>
+    <section class="bg-gray-50 pt-4">
+        <div>
+            <!-- Start coding here -->
+            <div class="bg-white relative border sm:rounded-lg overflow-hidden">
+                <div class="flex items-center justify-between p-4 border-b">
+                    <h1 class="text-[20px] font-bold">User</h1>
+                    <div class="w-full sm:max-w-[200px] max-w-[150px]">
+                        <form class="flex items-center">
+                            <label for="simple-search" class="sr-only">Search</label>
+                            <div class="relative w-full">
+                                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                    <svg aria-hidden="true" class="w-5 h-5 text-gray-500" fill="currentColor"
+                                        viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd"
+                                            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                                <input type="text" id="simple-search"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full pl-10 p-2"
+                                    placeholder="Search">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="overflow-x-auto">
+                    <table class="w-full text-sm text-left">
+                        <thead class="text-xs border-b uppercase">
+                            <tr class="[&>*]:whitespace-nowrap">
+                                <th scope="col" class="px-4 py-3">User ID</th>
+                                <th scope="col" class="px-4 py-3 text-center">User Name</th>
+                                <th scope="col" class="px-4 py-3 text-center">E-Mail</th>
+                                <th scope="col" class="px-4 py-3 text-center">Mobile</th>
+                                <th scope="col" class="px-4 py-3 text-center">Join Date</th>
+                                <th scope="col" class="px-4 py-3 text-center">Performance</th>
+                                <th scope="col" class="px-4 py-3 text-center">Status</th>
+                                <th scope="col" class="px-4 py-3 flex justify-center">
+                                    <label class="inline-flex items-center cursor-pointer">
+                                        <input type="checkbox" class="sr-only peer">
+                                        <div
+                                            class="relative w-9 h-5 bg-gray-200 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-blue">
+                                        </div>
+                                    </label>
+                                    <span class="sr-only">Actions</span>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="[&>*]:whitespace-nowrap" :class="i < max ? 'border-b' : ''" v-for="i in max"
+                                :key="i">
+                                <td scope="row" class="px-4 py-3">
+                                    <div class="flex items-center gap-4">
+                                        <div class="border w-8 aspect-square rounded-full overflow-hidden">
+                                            <img src="https://www.shutterstock.com/image-vector/woman-modern-icon-avatar-design-260nw-2358837033.jpg"
+                                                class="w-full h-full object-cover" alt="avtar">
+                                        </div>
+                                        <p>#0001</p>
+                                    </div>
+                                </td>
+                                <td class="px-4 py-3 text-center">Nitesh N Nageshri</td>
+                                <td class="px-4 py-3 text-center">nitesh@gmail.com</td>
+                                <td class="px-4 py-3 text-center">+91 85865 86969</td>
+                                <td class="px-4 py-3 text-center">24 Nov 2021</td>
+                                <td
+                                    class="px-4 py-3 text-center text-success font-semibold flex justify-center items-center">
+                                    12%
+                                    <svg class="w-3 h-3 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                        fill="none" viewBox="0 0 10 14">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2" d="M5 13V1m0 0L1 5m4-4 4 4" />
+                                    </svg>
+                                </td>
+                                <td class="px-4 py-3 text-center">
+                                    <!-- <span class="text-[12px] border text-blue rounded-md border-blue py-1 px-2" >Complete</span> -->
+                                    <!-- <span class="text-[12px] border text-warning rounded-md border-warning py-1 px-2" >Pending</span> -->
+                                    <span
+                                        class="text-[12px] border text-success rounded-md border-success py-1 px-2">Active</span>
+                                </td>
+
+                                <td class="px-4 pl-1 py-3 relative">
+                                    <div class="flex items-center justify-end gap-4">
+                                        <label class="inline-flex items-center cursor-pointer">
+                                            <input type="checkbox" class="sr-only peer">
+                                            <div
+                                                class="relative w-9 h-5 bg-gray-200 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-blue">
+                                            </div>
+                                        </label>
+                                        <button
+                                            class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg"
+                                            @click="showActionMenu(i)" v-click-outside="() => hideActionMenu(i)"
+                                            type="button">
+                                            <svg class="w-5 h-5 rotate-90" aria-hidden="true" fill="currentColor"
+                                                viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                <path
+                                                    d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+                                            </svg>
+                                        </button>
+                                        <div class="absolute z-10 w-44 bg-white border rounded-md divide-y divide-gray-100"
+                                            v-show="isShowActionMenu == i" :class="10 - 3 < i ? 'bottom-full' : 'top-full'">
+                                            <ul class="py-1 text-sm text-gray-700">
+                                                <li>
+                                                    <a href="#" class="block py-2 px-4 hover:bg-gray-100">Show</a>
+                                                </li>
+                                                <li>
+                                                    <a href="#" class="block py-2 px-4 hover:bg-gray-100">Edit</a>
+                                                </li>
+                                            </ul>
+                                            <div class="py-1">
+                                                <a href="#"
+                                                    class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100">Delete</a>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
--- src/views/App/students/index.vue
+++ src/views/App/students/index.vue
@@ -1,42 +1,19 @@
 <script setup>
-import StudentCard from './components/StudentCard.vue'
-import Btn from '@/components/Btn.vue';
+import Btn from '@/components/Btn.vue'
+import UserTable from './components/UserTable.vue';
 </script>
+
 <template>
-    <div>
-        <div class="flex justify-between items-center">
-            <h1 class="text-[24px] font-semibold">Users List</h1>
-            <Btn class="text-white bg-gray-950 font-medium rounded-md text-sm px-5 py-2.5 text-center">
-                Add Users
+    <div class="max-w-screen-xl mx-auto">
+        <div class="flex justify-between items-center" >
+            <h1 class="text-[24px] font-semibold">Users</h1>
+            <Btn class="text-white bg-gray-950 font-medium rounded-md text-sm px-4 py-2 text-center">
+                Add User
             </Btn>
         </div>
-        <div class="pt-10 flex justify-end items-center">
 
-            <div>
-                <Btn class="text-white bg-gray-950 font-medium rounded-md text-sm py-2.5 px-3.5 text-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="10" fill="currentColor" class="text-white"
-                        viewBox="0 0 320 512">
-                        <path
-                            d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z" />
-                    </svg>
-                </Btn>
-            </div>
-            <div class="relative ml-4">
-                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
-                        viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                            clip-rule="evenodd" />
-                    </svg>
-                </div>
-                <input type="text" id="simple-search"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block pl-10 p-2"
-                    placeholder="Search">
-            </div>
+        <div class="mt-4" >
+            <UserTable :max="10"/>
         </div>
-    </div>
-    <div class="grid 5xl:grid-cols-5 xl:grid-cols-4 md:grid-cols-3 xsm:grid-cols-2 gap-4 mt-5">
-        <StudentCard v-for="i in 20" :key="i" />
     </div>
 </template>
