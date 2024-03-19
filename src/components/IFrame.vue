be292721b1229c08fe2b1ddb0f9238041d1f99b1
Urvish <mandaliyaurvishj7@gmail.com>
2024-03-20 00:23:32 +0530

    small changes

---
+++ src/components/IFrame.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const frame = ref(null)
+
+onMounted(
+    () => {
+        console.log(frame);
+    }
+)
+</script>
+<template>
+    <!-- <iframe ref="frame" class="w-full aspect-video" scrolling="no" seamless="seamless" srcdoc='<script src="https://cdn.tailwindcss.com"></script><img src="https://plus.unsplash.com/premium_photo-1667480556783-119d25e19d6e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZHVtbXl8ZW58MHx8MHx8fDA%3D" class="border w-full aspect-video object-cover rounded-lg" alt="certificate">' ></iframe> -->
+    <iframe ref="frame" class="w-full aspect-video border rounded-lg cursor-pointer select-none" scrolling="no" seamless="seamless" srcdoc='<script src="https://cdn.tailwindcss.com"></script><h1 class="text-xl cursor-pointer" >test</h1>' ></iframe>
+</template>
--- src/views/App/result/index.vue
+++ src/views/App/result/index.vue
@@ -1,11 +1,16 @@
+<script setup>
+import IFrame from '@/components/IFrame.vue'
+</script>
+
 <template>
     <div class="max-w-screen-xl mx-auto">
         <h1 class="text-[24px] font-semibold">Results</h1>
 
-        <div class="mt-5 grid xl:grid-cols-5 md:grid-cols-4 grid-cols-2 gap-4" >
-            <div v-for="i in 5" class="rounded-lg overflow-hidden group cursor-pointer" >
+        <div class="mt-5 grid xl:grid-cols-3 md:grid-cols-4 grid-cols-2 gap-4" >
+            <div v-for="i in 3" class="rounded-lg overflow-hidden group cursor-pointer" >
                 <router-link to="#">
-                    <img src="@/assets/images/result.png" class="border aspect-[3.5/5] w-full rounded-lg" alt="certificate">
+                    <!-- <img src="@/assets/images/result.png" class="border aspect-[3.5/5] w-full rounded-lg" alt="certificate"> -->
+                    <IFrame/>
                     <div class="px-2 py-2" >
                         <p class="text-sm line-clamp-2 font-medium group-hover:underline">Exam 2024-25 result</p>
                     </div>
